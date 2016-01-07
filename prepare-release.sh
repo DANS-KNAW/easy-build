31b38b33f6ba09ff0c67b01f057e578d1fd40871
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2016-01-07 11:09:16 +0100

    Removed some unused code

--- prepare-release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-./prepare.sh easy-archive-bag || exit
-./prepare.sh easy-deposit || exit
-./prepare.sh easy-fcrepo-validate-checksums || exit
-./prepare.sh easy-ingest || exit
-./prepare.sh easy-stage-dataset || exit
-./prepare.sh easy-update-fs-rdb || exit
-./prepare.sh easy-update-solr-index || exit
